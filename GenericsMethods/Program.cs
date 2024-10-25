1037a54653c3c06f6cbc4f50eb9664dc49663ecd
VÍCTOR NÚÑEZ VICENTE <109703746+vnunez94@users.noreply.github.com>
2024-10-25 19:53:29 -0400

    add
    Add Generic Methods

---
+++ GenericsMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿int x = 10, y = 20;
+string s1 = "Manza", s2 = "Pera";
+
+Console.WriteLine($"x={x}, y={y}");
+Console.WriteLine($"s1={s1}, s2={s2}");
+
+Utility.Swap<int>(ref x, ref y);
+Utility.Swap<string>(ref s1, ref s2);
+Console.WriteLine();
+Console.WriteLine($"x={x}, y={y}");
+Console.WriteLine($"s1={s1}, s2={s2}");
+
+
+
+public static class Utility
+{
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T tmp=a; 
+        a=b; 
+        b=tmp;
+    }
+}
